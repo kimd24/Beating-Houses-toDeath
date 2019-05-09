9a299f4d02a80a27a03dda2e99feba7f3a1013fd
Tony Kim <kimd24@tcnj.edu>
2019-05-09 19:16:56 -0400

    add index for buildings

--- code/beatingHouses/config/routes.rb
+++ code/beatingHouses/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'buildings/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'buildings#index'
+  resources 'buildings'
 end
